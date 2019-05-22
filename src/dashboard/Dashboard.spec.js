542aa35dd8b9edfb3ea33ce5a84b2deee1cc0a94
sriley <sarahriley.dev@gmail.com>
2019-05-22 18:02:50 -0400

    mvp?

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Dashboard from "./Dashboard";
+
+describe.skip("<Dashboard />", () => {
+  it("snapshot", () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
